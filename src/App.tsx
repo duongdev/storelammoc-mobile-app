6e9c013135d1292fd069182c775bb16387b49b2d
Đỗ Cao Thượng Dương <dustin.do95@gmail.com>
2018-10-06 20:10:14 +0700

    feat(typescript): Add typescript support

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class App extends React.Component<any, any> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Hello World</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
