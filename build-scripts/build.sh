65e03b2417c3771560ca9360d86b20828e212417
Dương Đỗ <dustin.do95@gmail.com>
2018-11-22 13:23:24 +0700

    feat(travis): add scripts to build & deploy android app

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -10,10 +10,28 @@ yarn publish:$BUILD_ENV
 
 # Build native code
 if [ $BUILD_NATIVE = true ]; then
-  yarn build:$BUILD_ENV:ios
+  yarn build:$BUILD_ENV:ios & \
+  yarn build:$BUILD_ENV:\android
 
-  curl -o app.ipa "$(npx expo url:ipa --non-interactive)"
+  curl -o app-$BUILD_ENV-$BUILD_NUMBER.ipa "$(npx expo url:ipa --non-interactive)" &\
+  curl -o app-$BUILD_ENV-$BUILD_NUMBER.apk "$(npx expo url:apk --non-interactive)"
   
   sudo gem install fastlane
-  fastlane deliver --verbose --ipa "app.ipa" --skip_metadata --skip_screenshots --force
+
+  if [ $BUILD_ENV = "production" ]; then
+    # Upload apk to Play Store
+    fastlane supply \
+      --track "$BUILD_ENV" \
+      --json_key api-android-playstore.json \
+      --package_name "com.lammoc.store" \
+      --apk "app-$BUILD_ENV-$BUILD_NUMBER.apk" \
+      --skip_upload_metadata --skip_upload_images --skip_upload_screenshots
+  fi
+
+  # Upload app ipa to App Store Connect
+  fastlane deliver \
+    --verbose \
+    --ipa "app-$BUILD_ENV-$BUILD_NUMBER.ipa" \
+    --skip_metadata --skip_screenshots \
+    --force
 fi
