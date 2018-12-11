8923f27c2f03504336fab861f634fd995240d3ae
Dương Đỗ <dustin.do95@gmail.com>
2018-12-11 11:02:56 +0700

    feat(ci): add BUILD_VERSION env variable

--- build-scripts/env.sh
+++ build-scripts/env.sh
@@ -5,6 +5,7 @@ export TAG=$TRAVIS_TAG
 export BUILD_REV="$(cut -d'-' -f1 <<<"$TAG")"
 export BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
 export BUILD_ENV="$(cut -d'-' -f2 <<<"$TAG")"
+export BUILD_VERSION="$(cat version.txt)"
 
 if test "$(cut -d'-' -f3 <<<"$TAG")" = "native"; then
   export BUILD_NATIVE=true
